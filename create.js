28c5fdef891e26bf63a209514895b74c2ef2a019
lukas.droste <lukas.droste@vocanto.de>
2021-03-11 22:21:42 +0100

    sd

--- create.js
+++ create.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
-let bigArray = Array.from({length: 10000}, () => Math.floor(Math.random() * 10));
+let bigArray = Array.from({length: 9999}, () => Math.floor(Math.random() * 10));
 fs.writeFileSync('db.json', JSON.stringify(bigArray));
